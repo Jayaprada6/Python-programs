facb8010081178d8967a3aa5c0bbcf7c77ba341e
Jayaprada6 <jayaprada.aru@gmail.com>
2025-02-04 23:07:55 +0530

    dice.py

--- dice.py
+++ dice.py
@@ -1,11 +1,12 @@
 import random
-reply=input("Roll the dice?(yes/no) ")
-reply_low=reply.lower()
-if reply_low in ('yes'or'y'):
-    dice_num=[1,2,3,4,5,6]
-    ans=random.choices(dice_num,k=2)
-    print(*ans,sep=",")
-elif reply_low in ('no'or'n'):
-    print("Thanks for Playing")
-else:
-    print("Invalid Choice, give 'yes or no'")
+while True:
+    reply=input("Roll the dice?(yes/no) ").lower()
+    if reply in ('yes'or'y'):
+        dice_num=[1,2,3,4,5,6]
+        ans=random.choices(dice_num,k=2)
+        print(*ans,sep=",")
+    elif reply in ('no'or'n'):
+        print("Thanks for Playing")
+        break
+    else:
+        print("Invalid Choice, give 'yes or no'")
